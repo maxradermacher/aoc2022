c760087ca198392462c1a1b5141e3647fa0aed4b
Max Radermacher <max.radermacher@mxrmr.com>
2022-12-02 00:23:04 -0500

    Optimize 2-1

--- 02-1.py
+++ 02-1.py
@@ -2,13 +2,9 @@ import sys
 
 score = 0
 for line in sys.stdin:
-    line = line.rstrip()
-    them, _, us = line
+    them, _, us = line.rstrip()
     them, us = "ABC".index(them), "XYZ".index(us)
     score += us + 1
-    if them == us:
-        score += 3
-    elif (them + 1) % 3 == us:
-        score += 6
+    score += (us - them + 1 + 3) % 3 * 3
 
 print(score)
