037e3536bab3f9118ab6064f8ed3351407d5582f
Max Radermacher <max.radermacher@mxrmr.com>
2022-12-04 00:23:20 -0800

    Clean up Day 4

--- 04-1.py
+++ 04-1.py
@@ -1,11 +1,8 @@
 import sys
 
-score = 0
-for line in sys.stdin:
-    rs = [x.split("-") for x in line.rstrip().split(",")]
-    rs = sorted([(int(x[0]), int(x[1])) for x in rs])
-    if rs[1][0] >= rs[0][0] and rs[1][1] <= rs[0][1]:
-        score += 1
-    elif rs[0][0] >= rs[1][0] and rs[0][1] <= rs[1][1]:
-        score += 1
-print(score)
+res = 0
+for l in sys.stdin:
+    r1, r2 = [tuple(int(v) for v in r.split("-")) for r in l.rstrip().split(",")]
+    if (r1[0] >= r2[0] and r1[1] <= r2[1]) or (r2[0] >= r1[0] and r2[1] <= r1[1]):
+        res += 1
+print(res)
