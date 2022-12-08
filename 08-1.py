1c1b2eb44b0058cd6e4714548251a7857e7a3518
Max Radermacher <max.radermacher@mxrmr.com>
2022-12-07 22:21:29 -0800

    Clean up Day 8

--- 08-1.py
+++ 08-1.py
@@ -1,38 +1,16 @@
 import sys
 
-lines = sys.stdin.read().rstrip().split("\n")
-
-vis = []
-for l in lines:
-    vis.append([False] * len(l))
-
-
-iterations = [
-    (range(0, len(lines), 1), range(0, len(lines[0]), 1)),
-    (range(0, len(lines), 1), range(len(lines[0]) - 1, -1, -1)),
-    (range(len(lines) - 1, -1, -1), range(0, len(lines[0]), 1)),
-    (range(len(lines) - 1, -1, -1), range(len(lines[0]) - 1, -1, -1)),
+t = [list(map(int, list(line))) for line in sys.stdin.read().rstrip().split("\n")]
+w, h = len(t[0]), len(t)
+visible = [[False] * w for _ in range(0, h)]
+sight_paths = [
+    *[[(row, col) for col in range(0, w)] for row in range(0, h)],
+    *[[(row, col) for row in range(0, h)] for col in range(0, w)],
 ]
-
-rows = range(0, len(lines))
-cols = range(0, len(lines[0]))
-
-for row in rows:
-    for order in [cols, reversed(cols)]:
-        tallest = -1
-        for col in order:
-            height = int(lines[row][col])
-            if height > tallest:
-                tallest = height
-                vis[row][col] = True
-
-for col in cols:
-    for order in [rows, reversed(rows)]:
-        tallest = -1
-        for row in order:
-            height = int(lines[row][col])
-            if height > tallest:
-                tallest = height
-                vis[row][col] = True
-
-print(sum([1 if v else 0 for row in vis for v in row]))
+for sight_path in sight_paths + list(map(reversed, sight_paths)):
+    tallest = -1
+    for (row, col) in sight_path:
+        if t[row][col] > tallest:
+            tallest = t[row][col]
+            visible[row][col] = True
+print(sum([1 if v else 0 for r in visible for v in r]))
