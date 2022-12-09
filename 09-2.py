bc421e8b3f4e6136b3e69c241b8f814d2d5de4b0
Max Radermacher <max.radermacher@mxrmr.com>
2022-12-08 22:50:49 -0800

    Clean up Day 9

--- 09-2.py
+++ 09-2.py
@@ -1,25 +1,20 @@
 import sys
 
 
-DIRECTION = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-
 def correction(head, tail):
     diff = (head[0] - tail[0], head[1] - tail[1])
-    if abs(diff[0]) >= 2:
-        return (diff[0] // abs(diff[0]), diff[1] // max(1, abs(diff[1])))
-    if abs(diff[1]) >= 2:
-        return (diff[0] // max(1, abs(diff[0])), diff[1] // abs(diff[1]))
+    if abs(diff[0]) >= 2 or abs(diff[1]) >= 2:
+        return (diff[0] // max(1, abs(diff[0])), diff[1] // max(1, abs(diff[1])))
     return (0, 0)
 
 
 pos = set()
-rope = [(0, 0)] * 10
 pos.add((0, 0))
+rope = [(0, 0)] * 10
 for l in sys.stdin:
     direction, distance = l.rstrip().split(" ")
     for _ in range(int(distance)):
-        delta = DIRECTION["URDL".index(direction)]
+        delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]["URDL".index(direction)]
         for idx in range(len(rope) - 1):
             rope[idx] = (rope[idx][0] + delta[0], rope[idx][1] + delta[1])
             delta = correction(rope[idx], rope[idx + 1])
