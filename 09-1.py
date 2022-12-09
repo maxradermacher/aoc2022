9d8aa7a6aae920abb35d6738b486b150f29efbcc
Max Radermacher <max.radermacher@mxrmr.com>
2022-12-08 22:50:49 -0800

    Clean up Day 9

--- 09-1.py
+++ 09-1.py
@@ -1,17 +1,14 @@
 import sys
 
-
-DIRECTION = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-pos = set((0, 0))
+pos = set()
+pos.add((0, 0))
 head, tail = (0, 0), (0, 0)
 for l in sys.stdin:
     direction, distance = l.rstrip().split(" ")
-    for _ in range(0, int(distance)):
-        delta = DIRECTION["URDL".index(direction)]
+    for _ in range(int(distance)):
+        delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]["URDL".index(direction)]
         head = (head[0] + delta[0], head[1] + delta[1])
-        print(tail)
         if abs(head[0] - tail[0]) >= 2 or abs(head[1] - tail[1]) >= 2:
             tail = (head[0] - delta[0], head[1] - delta[1])
             pos.add(tail)
-        print(tail)
+print(len(pos))
