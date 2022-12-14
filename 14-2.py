80ecd628c727e18f49be1111e690796ff5dd0148
Max Radermacher <max.radermacher@mxrmr.com>
2022-12-13 23:16:10 -0800

    Clean up Day 14

--- 14-2.py
+++ 14-2.py
@@ -11,18 +11,30 @@ for l in sys.stdin:
             f.update([(n, s[1]) for n in range(min(s[0], e[0]), max(s[0], e[0]) + 1)])
         m = max(m, s[1], e[1])
 
-while (500, 0) not in f:
+
+def drop_sand(pos):
+    if pos[1] == m + 1:
+        return None
+    for delta in [(0, 1), (-1, 1), (1, 1)]:
+        new = (pos[0] + delta[0], pos[1] + delta[1])
+        if new in f:
+            continue
+        return new
+    return None
+
+
+def place_sand():
     pos = (500, 0)
     while True:
-        moved = False
-        for delta in [(0, 1), (-1, 1), (1, 1)]:
-            new = (pos[0] + delta[0], pos[1] + delta[1])
-            if new not in f and new[1] < m + 2:
-                pos = new
-                moved = True
-                break
-        if moved:
-            continue
-        f.add(pos)
-        print(pos)
-        break
+        new = drop_sand(pos)
+        if new is None:
+            f.add(pos)
+            return
+        pos = new
+
+
+n = 0
+while (500, 0) not in f:
+    place_sand()
+    n += 1
+print(n)
